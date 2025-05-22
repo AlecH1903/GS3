e10146f33d465d15a0fe75c8204064ace5bb52f1
Alec <alechadedbv@gmail.com>
2025-05-22 16:31:40 -0300

    Add files via upload

--- verda volt/model3.php
+++ verda volt/model3.php
@@ -19,8 +19,13 @@ include_once("code/loginC.php");
   <link rel="stylesheet" href="css/carrossel.css">
 </head>
 
-<header>
+
    
+  
+
+
+
+<body scrolling="no"  >
    <nav class="nav-elegante">
  <div class="nav-container">
    <a href="verdavolt.php" class="nav-logo">VerdaV</a>
@@ -50,10 +55,6 @@ include_once("code/loginC.php");
  </div>
 </nav>
 
-</header>
-
-
-<body>
   <div class="main-layout">
     <div class="background-section">
     <section class="section" id="carga">
@@ -100,64 +101,31 @@ include_once("code/loginC.php");
 
   </section>
     </div>
-    <div class="info-section">
-    <p style="overflow: scroll;">Em linguística, a noção de texto é ampla e ainda aberta a uma definição mais precisa. Grosso modo, pode ser entendido como manifestação linguística das ideias de um autor, que serão interpretadas pelo leitor de acordo com seus conhecimentos linguísticos e culturais. Seu tamanho é variável.
-Definições
+    <section class="info-section" scrolling="yes">
+    
+    <div class="div-content">
+    <h2>Model 3</h2>
 
-Existem várias definições de texto, cada uma destacando diferentes aspectos de sua natureza e função:
+    <div class="container-lado">
+  <div class="item">  <h3> 363mi </h3> <p> <br>
+    Range (EPA est.)
+    </p></div>
+  <div class="item"><h3>125mph </h3><p><br>
+    Top Speed</p>
+    </div>
+  <div class="item"><h3>4.9sec </h3><p> <br>
+    0-60 mph</p></div>
+</div>
 
-    "Conjunto de palavras e frases articuladas, escritas sobre qualquer suporte";[1]
-
-    "Obra escrita considerada na sua redação original e autêntica (por oposição a sumário, tradução, notas, comentários, etc.)";[2]
-
-    "Um texto é uma ocorrência linguística, escrita ou falada de qualquer extensão, dotada de unidade sociocomunicativa, semântica e formal. É uma unidade de linguagem em uso".[3]
-
-Estudo do Texto
-
-O interesse pelo texto como objeto de estudo gerou vários trabalhos importantes de teóricos da Linguística Textual, que percorreram fases diversas cujas características principais eram transpor os limites da frase descontextualizada da gramática tradicional e ainda incluir os relevantes papéis do autor e do leitor na construção de textos.
-
-Um texto pode ser escrito ou oral e, em sentido lato, pode ser também não verbal.
-
-Texto crítico é uma produção textual que parte de um processo reflexivo e analítico gerando um conteúdo com crítica construtiva e bem fundamentada.
-
-Em artes gráficas, o texto é a parte verbal, linguística, por oposição às ilustrações.
-Estrutura do Texto
-
-Todo texto tem que ter alguns aspectos formais, ou seja, tem que ter estrutura, elementos que estabelecem relação entre si. Dentro dos aspectos formais temos a coesão e a coerência, que dão sentido e forma ao texto. "A coesão textual é a relação, a ligação, a conexão entre as palavras, expressões ou frases do texto".[4] A coerência está relacionada com a compreensão, a interpretação do que se diz ou escreve. Um texto precisa ter sentido, isto é, precisa ter coerência. Embora a coesão não seja condição suficiente para que enunciados se constituam em textos, são os elementos coesivos que lhes dão maior legibilidade e evidenciam as relações entre seus diversos componentes, a coerência depende da coesão.
-Textos literários e não literários
-Texto
-
-Os textos literários são aqueles que, em geral, têm o objetivo de emocionar o leitor, e para isso exploram a linguagem conotativa ou poética. Em geral, ocorre o predomínio da função emotiva e poética.
-
-Exemplos de textos literários: poemas, romances, contos, novelas.
-
-Os textos não literários pretendem informar o leitor de forma direta e objetiva, a partir de uma linguagem denotativa. A função referencial predomina-se nos textos não-literários.
-
-Exemplos de textos não-literários: notícias e reportagens jornalísticas, textos de livros didáticos de História, Geografia, Ciências, textos científicos em geral, receitas culinárias, bulas de remédio.
-Referências
-
-Camargo, Ana Maria de Almeida; Heloísa Liberalli Bellotto (orgs.) (1996). Dicionário de terminologia arquivística. São Paulo: Associação dos Arquivistas Brasileiros – Núcleo de São Paulo / Secretaria de Estado da Cultura – Departamento de Museus e Arquivos. p. 74
-Ferreira, Aurélio Buarque de Holanda (1999). Dicionário Aurélio eletrônico – Século XXI Versão 3.0 ed. São Paulo: Lexikon
-Costa Val, M. da Graça (1991). Redação e textualidade. São Paulo: Martins Fontes
-
-    Platão, Francisco; José Luiz Fiorin (1996). Para entender o texto: leitura e redação 3ª ed. São Paulo: Ática
-
-Bibliografia
-
-    Koch, Ingedore Grunfeld Villaça; Luis Carlos Travaglia (2002). Texto e coerência 8ª ed. São Paulo: Cortez
-
-Ícone de esboço 	Este artigo sobre linguística ou um linguista é um esboço. Você pode ajudar a Wikipédia expandindo-o.
-Categorias:
-
-    Linguística textualLinguísticaLinguística aplicada
-
-</p>
-  </div>
-    <script src="https://unpkg.com/lenis@1.3.3/dist/lenis.min.js"></script> 
-     <script src="js/carrossel.js"></script>
-     <script src="js/lenis.js"></script>
-   
+    
+    
+    
   
+  </div>
 
+    </section>
+    
+     <script src="js/carrossel.js"></script>
+    
 </body>
 </html>
