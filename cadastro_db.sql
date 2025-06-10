fe3270dd4560f3257a3c67515b8cca106c21a701
Alec <alechadedbv@gmail.com>
2025-06-10 00:41:38 -0300

    Update cadastro_db.sql

--- cadastro_db.sql
+++ cadastro_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09/06/2025 às 11:02
+-- Tempo de geração: 10/06/2025 às 05:40
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
